b18bc02a71deac8214380aad9bf2ac648c95691a
juyeon99 <107914398+juyeon99@users.noreply.github.com>
2024-05-27 23:37:11 +0900

    Update Application.java

--- ch06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/practice/question5/Application.java
+++ ch06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/practice/question5/Application.java
@@ -2,7 +2,7 @@ package com.ohgiraffers.practice.question5;
 
 public class Application {
     public static void main(String[] args) {
-        Laptop laptop = new Laptop("Dell","XPS 13", 16,512);
+        Laptop laptop = new Laptop("Dell","XPS 13",16,512);
         laptop.print();
     }
 }
